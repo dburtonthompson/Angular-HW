0c67d4ec2b25faefe50351fde8c869e9d74a9dd5
dburtonthompson <dburtonthompson@gmail.com>
2020-05-27 23:08:59 -0400

    2nd commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CompOneComponent } from './comp-one/comp-one.component';
+import { FilterComponent } from './filter/filter.component';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Home', component: HomeComponent },
+  { path: 'One', component: CompOneComponent },
+  { path: 'Filter', component: FilterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompOneComponent } from './comp-one/comp-one.component';
+import { FilterComponent } from './filter/filter.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompOneComponent,
+    FilterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
